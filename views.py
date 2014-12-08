fed20e336473447514b45032a883958c4bdc8cfb
Alex Luton <aluton@gmail.com>
2014-12-08 14:13:12 +0000

    Restrict BlogEntryList to specified limit

--- views.py
+++ views.py
@@ -14,6 +14,11 @@ class BlogEntryList(ListView):
     def get_queryset(self):
         return BlogEntry.objects.order_by('date_published').reverse()
 
+    def get_context_data(self, **kwargs):
+        context = super(BlogEntryList, self).get_context_data(**kwargs)
+        context['display_limit'] = ':50'
+        return context
+
 class BlogEntryView(DetailView):
     model = BlogEntry
 
